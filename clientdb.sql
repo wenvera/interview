1c81e56b57eba904121e7a756688760b0b3c5938
wenvera <114447972+wenvera@users.noreply.github.com>
2024-11-27 13:53:52 -0500

    Add files via upload

---
+++ clientdb.sql
@@ -0,0 +1,53 @@
+-- Drop a database
+drop database if exists ClientDB;
+
+-- Create a database
+create database ClientDB ;
+
+-- Drop the client_credentials table if it already exists
+drop table if exists client_credentials;
+
+-- Create the client_credentials table (an empty table)
+create table client_credentials (
+                                    id SERIAL PRIMARY KEY,
+                                    clientname varchar(100),
+                                    email varchar(100),
+                                    Password varchar(100),
+                                    created_on date
+);
+
+-- Clear the data in this table while keeping the table in database to avoid repeated insertion of same data.
+truncate client_credentials;
+
+-- Load data from a csv file into the table
+copy client_credentials (id, clientname, email, Password, created_on)
+    from '/Users/VeraLiu_1/Downloads/MOCK_DATA_Cleaned.csv'
+    delimiter ','
+    csv header;
+
+-- test whether the data has been successfully loaded into table
+select * from client_credentials limit 10;
+select count(id) from client_credentials ;
+
+-- environment
+select version();
+
+-- SQL script used to create the sql user credentials
+-- Connect to the PostgreSQL database
+\c clientdb;
+
+-- Create a new user with a password
+CREATE USER peteryang WITH PASSWORD 'password1234';
+
+-- Grant privileges to the user
+-- grant all privileges on the database:
+GRANT ALL PRIVILEGES ON DATABASE ClientDB TO peteryang;
+
+-- grant specific privileges to the user on a particular schema or table
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO peteryang;
+
+
+
+
+
+
